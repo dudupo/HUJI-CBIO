6c2bc3f1fd21359485f4687432f2c2c49841af64
shakkedriver <shakked95@gamil.com>
2021-10-25 19:47:22 +0300

    added the oad_matrix function wich read the matrix file in to a dict

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
-
-import matplotlib.pylab as plt
 import argparse
-import numpy as np
 from itertools import groupby
+
+
+def load_matrix(path: str):
+    """
+    this function reads the matrix file and returns a python dict object such that every 2-tuple of letters from the
+    alphabet is mapped to a float that is the value for the pair according to the file
+    :param path: the path to the file
+    :return: dict such that every 2-tuple of letters from the
+    alphabet is mapped to a float that is the value for the pair according to the file
+    """
+
+    mat = dict()
+    file = open(path, 'r')
+    alphabet = file.readline().split()
+    for row in alphabet:
+        line = file.readline().split()
+        char = line[0]
+        for val, col in zip(line[1:], alphabet):
+            mat[(char, col)] = float(val)
+    return mat
+
 
 def fastaread(fasta_name):
     """
@@ -19,11 +37,13 @@ def fastaread(fasta_name):
 
 
 def main():
+    a = fastaread("ex1/fastas/HomoSapiens-SHH.fasta").__next__()[1]
     parser = argparse.ArgumentParser()
     parser.add_argument('seq_a', help='Path to first FASTA file (e.g. fastas/HomoSapiens-SHH.fasta)')
     parser.add_argument('seq_b', help='Path to second FASTA file')
     parser.add_argument('--align_type', help='Alignment type (e.g. local)', required=True)
-    parser.add_argument('--score', help='Score matrix in.tsv format (default is score_matrix.tsv) ', default='score_matrix.tsv')
+    parser.add_argument('--score', help='Score matrix in.tsv format (default is score_matrix.tsv) ',
+                        default='score_matrix.tsv')
     command_args = parser.parse_args()
     if command_args.align_type == 'global':
         raise NotImplementedError
@@ -36,5 +56,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
