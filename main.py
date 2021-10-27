f920f52c14d69f820f68fb60d97e5b7902653da1
dudupo <dudupo51@gmail.com>
2021-10-27 17:26:09 +0300

    fix bugs

--- main.py
+++ main.py
@@ -68,16 +68,17 @@ def global_base_case(seq1: str, seq2: str, mat: dict):
     :return:ndArray with sides the size of seq1 and seq2 and the values in the leftmost column would be acording to the
     alignment of the beginning of seq1 with '-'
     """
-
-    trace = np.ones( len(seq1), len(seq2))
-    cost  = np.zeros( len(seq1), len(seq2)) 
+    _maxlen = max(len(seq1), len(seq2))
+    _shape = (2*_maxlen + 1 , 2*_maxlen + 1)
+    trace = np.ones( _shape)
+    cost  = np.zeros(_shape)
 
     def reqursive_global(index1, index2):
-        
+        index1,index2 = int(index1),int(index2)
         options = [ ]
 
-        def check(index, seq):
-            return (index + 1) < len(seq)
+        def check(index, seq, otherindex):
+            return (index + 1) < len(seq) and (otherindex < 2 * max(len(seq1) , len(seq2)))
 
         def rightdown(options):
             reqursive_global(index1+1, index2+1)
@@ -95,39 +96,47 @@ def global_base_case(seq1: str, seq2: str, mat: dict):
         def right(options):
             reqursive_global(index1,   index2+1)
             options.append(
-                mat[('-', seq1[index2])] +
+                mat[('-', seq2[index2])] +
                  cost[index1][index2+1])
         def stop(options):
             pass
         
+        u =  (check(index1,seq1, index2) , check(index2,seq2, index1))
 
-        u =  (check(index1,seq1) , check(index2,seq2 ))
+        if u == (True,  True): 
+            rightdown(options)
+            down(options)
+            right(options)
+        elif u == (True,  False): 
+            options.append(-np.inf)
+            down(options)
+            options.append(-np.inf)
+        elif u == (False, True): 
+            options.append(-np.inf)
+            options.append(-np.inf)
+            right(options)
 
-        {
-            (True,  True)    : rightdown,
-            (True,  False)   : right,
-            (False, True)    : down,
-            (False, False)   : stop
-
-        }[u](options)
-
-        if len(options == 0):
+        if u == (False, False):
             return
         else :
             trace[index1][index2] = argmax(options)
             cost[index1][index2]  = max(options)
 
     reqursive_global(0,0)
+    print(seq1)
+    print(seq2)
+    print(cost)
+    print(trace)
+    return cost, trace
+    
+    # exit(1)
 
-    print(cost, trace)
-    exit(1)
-
-    shape = (len(seq1), len(seq2))
-    table = np.empty(shape, dtype=float)
-    table[0, 0] = mat[(seq1[0], '-')]
-    for row, char in enumerate(seq1[1:]):
-        table[row, 0] =  mat[(char, '-')] + table[row - 1, 0]
-    return table
+    # shape = (len(seq1), len(seq2))
+    # table = np.empty(shape, dtype=float)
+    # table[0, 0] = mat[(seq1[0], '-')]
+    # for row, char in enumerate(seq1[1:]):
+    #     table[row, 0] =  mat[(char, '-')] + table[row - 1, 0]
+    # return table
 
 
 def min_argmin(options: List[float]) -> tuple:
@@ -238,6 +247,10 @@ def func_NotImplementedError():
 
 
 import csv
+import sys
+
+sys.setrecursionlimit(50000)
+
 def parse_matrix(scorefile):
     ret = []
     with open(scorefile) as fd:
@@ -249,7 +262,7 @@ def parse_matrix(scorefile):
 
 def main():
     a = fastaread("ex1/fastas/HomoSapiens-SHH.fasta").__next__()[1]
-    print(a)
+    # print(a)
     parser = argparse.ArgumentParser()
     parser.add_argument('seq_a', help='Path to first FASTA file (e.g. fastas/HomoSapiens-SHH.fasta)')
     parser.add_argument('seq_b', help='Path to second FASTA file')
@@ -260,19 +273,17 @@ def main():
     
     # print(fastaread(command_args.seq_a).__next__()[1])
     seq_a, seq_b  = fastaread(command_args.seq_a).__next__()[1], fastaread(command_args.seq_b).__next__()[1]
-    print(seq_a, seq_b)
-    mat = parse_matrix(command_args.score)
+    # print(seq_a, seq_b)
+    mat = load_matrix(command_args.score)
     print(mat)
-    print( command_args.score)
-    
-    
+        
     #       --- To do ---- 
     #       -> handle the if condition, (doesn't work for me).
     #
     # if str(command_args.align_type) == 'global':
         
     
-    alignment = global_alignment(seq_a, seq_b, command_args.score)
+    alignment = global_alignment(seq_a[:5], seq_b[:5], mat)
 
     
 if __name__ == '__main__':
