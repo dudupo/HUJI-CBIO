6dffa41f98393f9bc1715887d4c70361bf438b90
dudupo <dudupo51@gmail.com>
2021-10-29 18:00:44 +0300

    fix bugs

--- main.py
+++ main.py
@@ -70,85 +70,12 @@ def global_base_case(seq1: str, seq2: str, mat: dict):
     """
     _maxlen = max(len(seq1), len(seq2))
     _shape =  (len(seq1), len(seq2)) #(2*_maxlen + 1 , 2*_maxlen + 1)
-    trace = np.ones( _shape)
+    trace = np.zeros( _shape)
     cost  = np.zeros(_shape)
-
-    def reqursive_global(index1, index2):
-        index1,index2 = int(index1),int(index2)
-        options = [ ]
-
-        def check(index, seq, otherindex):
-            return (index + 1) < len(seq) and (otherindex < 2 * max(len(seq1) , len(seq2)))
-
-        def rightdown(options):
-            reqursive_global(index1+1, index2+1)
-            options.append(
-                mat[(seq1[index1], seq2[index2])] +
-                 cost[index1+1][index2+1])
-        
-        def down(options):
-            reqursive_global(index1+1,   index2)
-            options.append(
-                mat[(seq1[index1], '-')] +
-                 cost[index1+1][index2])
-
-        
-        def right(options):
-            reqursive_global(index1,   index2+1)
-            options.append(
-                mat[('-', seq2[index2])] +
-                 cost[index1][index2+1])
-        def stop(options):
-            pass
-        
-        u =  (check(index1,seq1, index2) , check(index2,seq2, index1))
-
-        if u == (True,  True): 
-            rightdown(options)
-            down(options)
-            right(options)
-        elif u == (True,  False): 
-            options.append(-np.inf)
-            down(options)
-            options.append(-np.inf)
-        elif u == (False, True): 
-            options.append(-np.inf)
-            options.append(-np.inf)
-            right(options)
-        else:
-            return
-        # else :
-        cost[index1][index2]  = max(options)
-
-    reqursive_global(0,0)
-    
-    for i in reversed(range(len(seq1))):
-        for j in reversed(range(len(seq2))):
-            options = [ ]
-            
-            if i-1 >= 0 and j-1 >= 0:
-                options.append( cost[i-1][j-1] )
-            else:
-                options.append( -np.inf )
-            
-            if i >= 0 and j-1 >= 0:
-                options.append( cost[i][j-1] )
-            else:
-                options.append( -np.inf )
-            
-            if i-1 >= 0 and j >= 0:
-                options.append( cost[i-1][j] )
-            else:
-                options.append( -np.inf )
-            
-
-            trace[i][j] = argmax(options)
-
-    # trace[index1][index2] = argmax(options)
-    print(seq1)
-    print(seq2)
+    trace, cost = fill_tables_for_global(seq1, seq2, mat, cost, trace)
     print(cost)
     print(trace)
+    
     return cost, trace
     
     # exit(1)
@@ -167,7 +94,7 @@ def min_argmin(options: List[float]) -> tuple:
     :param options: a list with numbers
     :return: min,argmin of the list
     """
-    return np.min(options), np.argmin(options)
+    return np.max(options), np.argmax(options)
 
 
 def fill_cell_for_global(seq1: str, seq2: str, mat: dict, table, trace, i: int, j: int):
@@ -183,7 +110,7 @@ def fill_cell_for_global(seq1: str, seq2: str, mat: dict, table, trace, i: int, 
     :return:none
     """
     options = [table[i - 1, j] + mat[(seq1[i], '-')], table[i - 1, j - 1] + mat[(seq1[i], seq2[j])],
-               table[i, j - 1] + mat[(seq2[j], '-')]]
+               table[i, j - 1] + mat[('-', seq2[j])]]
     table_val, trace_val = min_argmin(options)
     table[i, j] = table_val
     trace[i, j] = trace_val
@@ -223,13 +150,13 @@ def extract_solution_global(seq1: str, seq2: str, mat: dict, table, trace):
     i, j =  table.shape[0] - 1, table.shape[1] - 1
     str1, str2 = "", ""
     while not i == j == 0:
-        if trace[i, j] == 1:
+        if trace[i, j] == 2:
             str1 += '-'
             str2 += seq2[j]
             j-=1
             continue
         # change
-        elif trace[i, j] == 0:
+        elif trace[i, j] == 1:
             str1 += seq1[i]
             str2 += seq2[j]
             j -= 1
@@ -301,7 +228,7 @@ def main():
     # if str(command_args.align_type) == 'global':
         
     
-    alignment = global_alignment(seq_a[:5], seq_b[:5], mat)
+    alignment = global_alignment(seq_a, seq_b, mat)
 
     
 if __name__ == '__main__':
