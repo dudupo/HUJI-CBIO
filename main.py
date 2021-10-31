4adf198bc878822e650350c130501ae34376f2b0
shakkedriver <shakked95@gamil.com>
2021-10-31 21:03:34 +0200

    done with the functions for overlap and local

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from itertools import groupby
 from typing import List
 
 import numpy as np
-from numpy.core.fromnumeric import argmax
 
 
 def load_matrix(path: str):
@@ -36,11 +35,76 @@ def local_base_case(seq1: str, seq2: str, mat: dict):
     :return:ndArray with sides the size of seq1 and seq2 and the values in the leftmost column and upper most row will 
     be 0 
     """
-    shape = (len(seq1), len(seq2))
+    shape = (len(seq1) + 1, len(seq2) + 1)
     table = np.empty(shape, dtype=float)
     table[:, 0] = 0
     table[0, :] = 0
-    return table
+    trace = np.ones(shape, dtype=np.int8) * 3
+    return table, trace
+
+
+def fill_cell_for_local(seq1, seq2, mat, table, trace, i, j):
+    options = [table[i - 1, j] + mat[(seq1[i - 1], '-')], table[i - 1, j - 1] + mat[(seq1[i - 1], seq2[j - 1])],
+               table[i, j - 1] + mat[('-', seq2[j - 1])], 0]
+    table_val, trace_val = max_argmax(options)
+    table[i, j] = table_val
+    trace[i, j] = trace_val
+    return
+
+
+def fill_tables_for_local(seq1: str, seq2: str, mat: dict, table, trace):
+    """
+    fill the whole table for the local aliment using fill_cell_for_local function and returns the trace and table for
+    seq1 seq2
+    :param seq1:the first sequence
+    :param seq2:the second sequence
+    :param mat: the score matrix
+    :param table: the dynamic programming table for the best scores of partial alignments
+    :param trace:the matrix we use to indicate what is the partial aliment we used in order to get to this alignment
+    :return:trace and table
+    """
+    for col in range(1, table.shape[1]):
+        for row in range(1, table.shape[0]):
+            fill_cell_for_local(seq1, seq2, mat, table, trace, row, col)
+    return trace, table
+
+
+def extract_solution_local(seq1: str, seq2: str, mat: dict, table, trace):
+    """
+    extract the optimal local alignment for sqe1 seq2 based on table, trace
+    :param seq1:the first sequence
+    :param seq2:the second sequence
+    :param mat: the score matrix
+    :param table: the dynamic programming table for the best scores of partial alignments
+    :param trace:the matrix we use to indicate what is the partial aliment we used in order to get to this alignment
+    :return:trace and table
+    :return: two strings that are sqe1 and seq2 with '-' in them according to the optimal global alignment
+    """
+    i, j = np.unravel_index(table.argmax(), table.shape())
+    str1, str2 = "", ""
+    while i > 0 and j > 0:
+        if trace[i, j] == 2:
+            str1 += '-'
+            str2 += seq2[j - 1]
+            j -= 1
+            continue
+        # change
+        elif trace[i, j] == 1:
+            str1 += seq1[i - 1]
+            str2 += seq2[j - 1]
+            j -= 1
+            i -= 1
+            continue
+        elif trace[i, j] == 2:
+            str1 += seq1[i - 1]
+            str2 += '-'
+            i -= 1
+            continue
+        else:  # trace[i, j] == 3
+            str1 += seq1[i - 1]
+            str2 += seq2[j - 1]
+            break
+    return str1[::-1], str2[::-1]
 
 
 def overlap_base_case(seq1: str, seq2: str, mat: dict):
@@ -68,15 +132,16 @@ def global_base_case(seq1: str, seq2: str, mat: dict):
     :return:ndArray with sides the size of seq1 and seq2 and the values in the leftmost column would be acording to the
     alignment of the beginning of seq1 with '-'
     """
-    shape = (len(seq1)+1, len(seq2)+1)
-    trace = np.zeros(shape) #  np.empty(shape,dtype=np.int8)
-    trace[:,0] =0
-    cost = np.zeros(shape) # np.empty(shape, dtype=float)
-    cost[0, 0] = 0 #mat[(seq1[0], '-')]
+    shape = (len(seq1) + 1, len(seq2) + 1)
+    trace = np.zeros(shape)  # np.empty(shape,dtype=np.int8)
+    trace[:, 0] = 0
+    cost = np.zeros(shape)  # np.empty(shape, dtype=float)
+    cost[0, 0] = 0  # mat[(seq1[0], '-')]
     for row, char in enumerate(seq1):
-        cost[row+1, 0] =  mat[(char, '-')] + cost[row, 0]
+        cost[row + 1, 0] = mat[(char, '-')] + cost[row, 0]
     trace, cost = fill_tables_for_global(seq1, seq2, mat, cost, trace)
     return cost, trace
+
 
 def max_argmax(options: List[float]) -> tuple:
     """
@@ -84,7 +149,8 @@ def max_argmax(options: List[float]) -> tuple:
     :param options: a list with numbers
     :return: min,argmin of the list
     """
-    return np.max(options), np.argmax(options)
+    argmax = np.argmax(options)
+    return options[argmax], argmax
 
 
 def fill_cell_for_global(seq1: str, seq2: str, mat: dict, table, trace, i: int, j: int):
@@ -99,8 +165,8 @@ def fill_cell_for_global(seq1: str, seq2: str, mat: dict, table, trace, i: int, 
     :param j:the column of the cell we wish to calculate
     :return:none
     """
-    options = [table[i - 1, j] + mat[(seq1[i-1], '-')], table[i - 1, j - 1] + mat[(seq1[i-1], seq2[j-1])],
-               table[i, j - 1] + mat[('-', seq2[j-1])]]
+    options = [table[i - 1, j] + mat[(seq1[i - 1], '-')], table[i - 1, j - 1] + mat[(seq1[i - 1], seq2[j - 1])],
+               table[i, j - 1] + mat[('-', seq2[j - 1])]]
     table_val, trace_val = max_argmax(options)
     table[i, j] = table_val
     trace[i, j] = trace_val
@@ -119,9 +185,42 @@ def fill_tables_for_global(seq1: str, seq2: str, mat: dict, table, trace):
     """
     for col in range(1, table.shape[1]):
         trace[0, col] = 2
-        table[0, col] = table[0, col - 1] + mat[(seq2[col-1], '-')]
+        table[0, col] = table[0, col - 1] + mat[(seq2[col - 1], '-')]
         for row in range(1, table.shape[0]):
             fill_cell_for_global(seq1, seq2, mat, table, trace, row, col)
+    return trace, table
+
+
+def fill_cell_for_ovarlap(seq1, seq2, mat, table, trace, i, j):
+    if i!=table.shape[0]-1:
+        options = [table[i - 1, j] + mat[(seq1[i - 1], '-')], table[i - 1, j - 1] + mat[(seq1[i - 1], seq2[j - 1])],
+                table[i, j - 1] + mat[('-', seq2[j - 1])]]
+    else:
+        options = [table[i - 1, j] + mat[(seq1[i - 1], '-')], table[i - 1, j - 1] + mat[(seq1[i - 1], seq2[j - 1])],
+                   table[i, j - 1]]
+    table_val, trace_val = max_argmax(options)
+    table[i, j] = table_val
+    trace[i, j] = trace_val
+
+
+
+
+def fill_tables_for_overlap(seq1: str, seq2: str, mat: dict, table, trace):
+    """
+    fill the whole table for the overlap aliment using fill_cell_for_global function and returns the trace and table for
+    seq1 seq2
+    :param seq1:the first sequence
+    :param seq2:the second sequence
+    :param mat: the score matrix
+    :param table: the dynamic programming table for the best scores of partial alignments
+    :param trace:the matrix we use to indicate what is the partial aliment we used in order to get to this alignment
+    :return:trace and table
+    """
+    for col in range(1, table.shape[1]):
+        trace[0, col] = 2
+        table[0, col] = table[0, col - 1] + mat[(seq2[col - 1], '-')]
+        for row in range(1, table.shape[0]):
+            fill_cell_for_ovarlap(seq1, seq2, mat, table, trace, row, col)
     return trace, table
 
 
@@ -137,33 +236,51 @@ def extract_solution_global(seq1: str, seq2: str, mat: dict, table, trace):
     :return:trace and table
     :return: two strings that are sqe1 and seq2 with '-' in them according to the optimal global alignment
     """
-    i, j =  table.shape[0] - 1, table.shape[1] - 1
+    i, j = table.shape[0] - 1, table.shape[1] - 1
     str1, str2 = "", ""
-    while  i > 0 and j > 0:
+    while i > 0 and j > 0:
         if trace[i, j] == 2:
             str1 += '-'
-            str2 += seq2[j-1]
-            j-=1
+            str2 += seq2[j - 1]
+            j -= 1
             continue
         # change
         elif trace[i, j] == 1:
-            str1 += seq1[i-1]
-            str2 += seq2[j-1]
+            str1 += seq1[i - 1]
+            str2 += seq2[j - 1]
             j -= 1
-            i-= 1
+            i -= 1
             continue
         else:
-            str1 += seq1[i-1]
+            str1 += seq1[i - 1]
             str2 += '-'
-            i-= 1
+            i -= 1
             continue
-    return str1[::-1],str2[::-1]
+    return str1[::-1], str2[::-1]
 
-def extract_solution_local(seq1: str, seq2: str, mat: dict, table, trace):
-    pass 
 
 def extract_solution_overlap(seq1: str, seq2: str, mat: dict, table, trace):
-    pass 
+    i, j = table.shape[0] - 1, np.argmax(table[table.shape[0] - 1])
+    str1, str2 = "", ""
+    while j > 0:
+        if trace[i, j] == 2:
+            str1 += '-'
+            str2 += seq2[j - 1]
+            j -= 1
+            continue
+        # change
+        elif trace[i, j] == 1:
+            str1 += seq1[i - 1]
+            str2 += seq2[j - 1]
+            j -= 1
+            i -= 1
+            continue
+        else:
+            str1 += seq1[i - 1]
+            str2 += '-'
+            i -= 1
+            continue
+    return str1[::-1], str2[::-1]
 
 
 def fastaread(fasta_name):
@@ -179,36 +296,40 @@ def fastaread(fasta_name):
         seq = "".join(s.strip() for s in next(faiter))
         yield header, seq
 
-def general_alignment( seq_a, seq_b, mat, base_case_func, extract_func):
+
+def general_alignment(seq_a, seq_b, mat, base_case_func, extract_func):
     def split_to_lines(_str):
         chunks, chunk_size = len(_str), 50
-        return [ _str[i:i+chunk_size] for i in range(0, chunks, chunk_size) ]
+        return [_str[i:i + chunk_size] for i in range(0, chunks, chunk_size)]
 
     table, trace = base_case_func(seq_a, seq_b, mat)
-    ret_seq1, ret_seq2 = extract_func( seq_a, seq_b, mat, table, trace )
-    for upper_line, bottom_line in\
-         zip( split_to_lines(ret_seq1), split_to_lines(ret_seq2) ):
+    ret_seq1, ret_seq2 = extract_func(seq_a, seq_b, mat, table, trace)
+    for upper_line, bottom_line in \
+            zip(split_to_lines(ret_seq1), split_to_lines(ret_seq2)):
         print(upper_line)
         print(bottom_line)
         print()
     return table
-    
+
+
 def global_alignment(seq_a, seq_b, mat):
-    table = general_alignment(seq_a, seq_b, mat,\
-         global_base_case, extract_solution_global)
-    print("global:{0}".format(table[-1,-1]))
+    table = general_alignment(seq_a, seq_b, mat, \
+                              global_base_case, extract_solution_global)
+    print("global:{0}".format(table[-1, -1]))
+
 
 def local_alignment(seq_a, seq_b, mat):
-    table = general_alignment(seq_a, seq_b, mat,\
-         local_base_case, extract_solution_local)
-    print("local:{0}".format(table[-1,-1]))
+    table = general_alignment(seq_a, seq_b, mat, \
+                              local_base_case, extract_solution_local)
+    print("local:{0}".format(table[-1, -1]))
+
 
 def overlap_alignment(seq_a, seq_b, mat):
-    table = general_alignment(seq_a, seq_b, mat,\
-         overlap_base_case, extract_solution_overlap)
-    print("overlap:{0}".format(table[-1,-1]))
+    table = general_alignment(seq_a, seq_b, mat, \
+                              overlap_base_case, extract_solution_overlap)
+    print("overlap:{0}".format(table[-1, -1]))
 
-import csv
+
 import sys
 
 sys.setrecursionlimit(50000)
@@ -221,17 +342,17 @@ def main():
     parser.add_argument('--align_type', help='Alignment type (e.g. local)', required=True)
     parser.add_argument('--score', help='Score matrix in.tsv format (default is score_matrix.tsv) ',
                         default='score_matrix.tsv')
-    
+
     parser.add_argument('--testlen', default=-1, required=False)
     command_args = parser.parse_args()
-    
-    seq_a, seq_b  = fastaread(command_args.seq_a).__next__()[1], fastaread(command_args.seq_b).__next__()[1]
-    seq_a, seq_b = seq_a[:int(command_args.testlen)], seq_b[:int(command_args.testlen)]    
+
+    seq_a, seq_b = fastaread(command_args.seq_a).__next__()[1], fastaread(command_args.seq_b).__next__()[1]
+    seq_a, seq_b = seq_a[:int(command_args.testlen)], seq_b[:int(command_args.testlen)]
     mat = load_matrix(command_args.score)
-    
+
     alignment = global_alignment(seq_a, seq_b, mat)
 
-    
+
 if __name__ == '__main__':
     main()
 0
